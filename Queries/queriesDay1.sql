7fccb2ce22a25e6b25db1190483a6b29f80fa1c6
Sumeyra <smyryalcin@gmail.com>
2023-03-14 10:59:26 +0000

    queries day 1 apdated

--- Queries/queriesDay1.sql
+++ Queries/queriesDay1.sql
@@ -1,1 +1,20 @@
 select * from EMPLOYEES;
+--reads all column from employees table
+
+select * from DEPARTMENTS;
+
+select FIRST_NAME from EMPLOYEES;
+--get only firstname from employees table
+
+--display city names
+select CITY from LOCATIONS;
+
+--get me firstname, lastname and salary
+select FIRST_NAME,LAST_NAME,SALARY from EMPLOYEES;
+--SHORTCUT to run command+enter
+
+--get me street address and postalcode
+select STREET_ADDRESS,POSTAL_CODE from LOCATIONS;
+
+select distinct FIRST_NAME from EMPLOYEES;
+--remove duplicates. distinct . if you want to see unique use distinct keyword.
