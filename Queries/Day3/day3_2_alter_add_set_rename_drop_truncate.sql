6d633662a7bd4c1e0842f52f5cb3d1a48c35db21
Sumeyra <smyryalcin@gmail.com>
2023-03-15 11:33:32 +0000

    queries day3 -1 truncate and drop table

--- Queries/Day3/day3_2_alter_add_set_rename_drop_truncate.sql
+++ Queries/Day3/day3_2_alter_add_set_rename_drop_truncate.sql
@@ -5,3 +5,36 @@ ALTER TABLE SCRUMTEAM add salary Integer;
 
 --update existing employees salary
 update SCRUMTEAM set salary =100000 where EMP_ID=1;
+
+update SCRUMTEAM set salary =90000 where EMP_ID=2;
+
+update SCRUMTEAM set salary =120000 where EMP_ID=4;
+
+--or we can add with or
+-- update SCRUMTEAM set salary =100000 where EMP_ID=1 or EMP_ID=2;
+select * from SCRUMTEAM;
+
+--rename the column
+ALTER TABLE SCRUMTEAM RENAME COLUMN salary To annual_salary;
+
+select * from SCRUMTEAM;
+
+--delete , drop column
+ALTER TABLE SCRUMTEAM DROP COLUMN annual_salary;
+
+select * from SCRUMTEAM;
+
+--how to change table name
+ALTER TABLE SCRUMTEAM RENAME TO agileteam;
+
+select * from agileteam;
+
+commit;
+
+--truncate, if we want to delete all data from the table,
+--but still keep the data structure, we use truncate
+TRUNCATE TABLE  agileteam;
+
+
+--if we want to delete the table and data together
+DROP TABLE agileteam;
