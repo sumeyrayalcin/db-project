7a5a5a2d64c2217d253468e925e3d2d0b586a15f
Sumeyra <smyryalcin@gmail.com>
2023-03-15 14:34:55 +0000

    queries day4 created left full outer

---
+++ Queries/Day4/day4_1_left_full_outerJOIN.sql
@@ -0,0 +1,43 @@
+--from dan sonra gelen left table
+--join den sonra gelen right table
+
+/*
+ LEFT OUTER JOIN WITH WHERE
+ only shows left table's unique ones
+ from customer left outer join address : sadece customer table da olanları gösterir,
+ diğer table ile common olanları değil.
+ */
+
+select * from EMPLOYEES;
+select * from DEPARTMENTS;
+
+select FIRST_NAME, LAST_NAME, DEPARTMENT_NAME
+from EMPLOYEES e left join DEPARTMENTS d
+on e.DEPARTMENT_ID = d.DEPARTMENT_ID;
+
+
+-- doesnt work well, I will check it later
+select CUSTOMER_ID,FIRST_NAME,LAST_NAME,CUSTOMER.ADDRESS_ID,ADDRESS.ADDRESS_ID ADDRESS,PHONE
+from CUSTOMER left outer join ADDRESS
+                              on CUSTOMER.ADDRESS_ID = ADDRESS.ADDRESS_ID
+where CUSTOMER.ADDRESS_ID IS NULL;
+
+--dilek
+select CUSTOMER_ID,FIRST_NAME,LAST_NAME,CUSTOMER.ADDRESS_ID,ADDRESS,PHONE
+from CUSTOMER left outer join  address
+                               on CUSTOMER.ADDRESS_ID=ADDRESS.address_ID
+where CUSTOMER.address_id is null;
+
+
+commit ;
+
+
+
+/*
+ FULL OUTER JOIN WITH WHERE
+ shows only unique ones from two tables. it doest show the common ones.
+ */
+select *from CUSTOMER FULL OUTER JOIN ADDRESS
+                                      On CUSTOMER.ADDRESS_ID=ADDRESS.ADDRESS_ID
+where CUSTOMER.ADDRESS_ID IS NULL OR
+    ADDRESS.ADDRESS_ID IS NULL ;
