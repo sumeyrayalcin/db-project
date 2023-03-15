fb13f3acd1f7a7cf83b704d39a2a250133a3e056
Sumeyra <smyryalcin@gmail.com>
2023-03-15 12:31:46 +0000

    queries day3_3_joinTable_left_right_full

--- Queries/Day3/day3_4_example.sql
+++ Queries/Day3/day3_4_example.sql
@@ -1,2 +1,9 @@
-select * from customer;
-select * from address;
+
+
+select * from EMPLOYEES;
+
+select * from DEPARTMENTS;
+
+select FIRST_NAME,LAST_NAME,DEPARTMENT_NAME
+from EMPLOYEES e left join DEPARTMENTS d
+                           on e.DEPARTMENT_ID=d.DEPARTMENT_ID;
