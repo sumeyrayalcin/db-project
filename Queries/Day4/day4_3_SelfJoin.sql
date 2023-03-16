89a93a7e1f14964bc50cbe4aa94b7ffc9a6467c9
Sumeyra <smyryalcin@gmail.com>
2023-03-16 10:53:59 +0000

    queries day4 self join created

---
+++ Queries/Day4/day4_3_SelfJoin.sql
@@ -0,0 +1,6 @@
+/*
+ SELF JOIN
+ when we join according to the same table's column
+ we use the same table two times, we seperate the same table in two different table
+ and create the logical condition
+ */
