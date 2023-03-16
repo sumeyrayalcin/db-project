a9d40054a517e4145326a3a7e8c4d4597fc09ed3
Sumeyra <smyryalcin@gmail.com>
2023-03-16 11:06:46 +0000

    queries day4 self join

--- Queries/Day4/day4_3_SelfJoin.sql
+++ Queries/Day4/day4_3_SelfJoin.sql
@@ -4,3 +4,11 @@
  we use the same table two times, we seperate the same table in two different table
  and create the logical condition
  */
+
+--get me all employees firstname, lastname and their managers first and lastname
+select EMPLOYEE_ID,FIRST_NAME,LAST_NAME,MANAGER_ID from EMPLOYEES;
+
+select e1.employee_id, e1.first_name, e1.LAST_NAME,e1.MANAGER_ID,e2.EMPLOYEE_ID,e2.FIRST_NAME,e2.LAST_NAME
+from EMPLOYEES e1 join EMPLOYEES e2
+                       on e1.MANAGER_ID=e2.EMPLOYEE_ID
+order by e1.EMPLOYEE_ID;
