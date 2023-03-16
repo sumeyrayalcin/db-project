42225388e0994190c76954afb941ce49d2b68819
Sumeyra <smyryalcin@gmail.com>
2023-03-16 11:13:04 +0000

    queries day4 self join done

--- Queries/Day4/day4_3_SelfJoin.sql
+++ Queries/Day4/day4_3_SelfJoin.sql
@@ -12,3 +12,11 @@ select e1.employee_id, e1.first_name, e1.LAST_NAME,e1.MANAGER_ID,e2.EMPLOYEE_ID,
 from EMPLOYEES e1 join EMPLOYEES e2
                        on e1.MANAGER_ID=e2.EMPLOYEE_ID
 order by e1.EMPLOYEE_ID;
+
+
+
+--with left join it will show the null value from left table
+select e1.EMPLOYEE_ID,e1.FIRST_NAME,e1.LAST_NAME,e1.MANAGER_ID,e2.EMPLOYEE_ID,e2.FIRST_NAME,e2.LAST_NAME
+from EMPLOYEES e1 left join EMPLOYEES e2
+                            on e1.MANAGER_ID=e2.EMPLOYEE_ID
+order by e1.EMPLOYEE_ID;
